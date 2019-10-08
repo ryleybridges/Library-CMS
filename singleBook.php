ac3416f0d3bc336474ae9e97246f9e106395bc83
ryleybridges <rye.brid@gmail.com>
2019-10-09 09:07:28 +1300

    Set up php templates, converted HTML to PHP and set up code

--- singleBook.php
+++ singleBook.php
@@ -1,43 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Yoobee School of Design Library</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/style.css">
-</head>
+<?php require('templates/header.php'); ?>
 <body>
-    <header class="blog-header py-3 bg-info">
-        <div class="container">
-            <div class="row text-center d-flex">
-                <div class="col-6 col-md-3 d-flex align-items-center justify-content-center justify-content-md-start order-2 order-md-1 pt-2 pt-md-0">
-                    <a class="text-light" href="#">Subscribe</a>
-                </div>
-                <div class="col-12 col-md order-1 order-md-2">
-                    <a class="blog-header-logo text-light" href="index.html">Yoobee School of Design Library</a>
-                </div>
-                <div class="col-6 col-md-3 d-flex align-items-center justify-content-center justify-content-md-end order-3 order-md-3 pt-2 pt-md-0">
-                    <a class="text-light" href="#">
-                        <i class="fas fa-search mx-3"></i>
-                    </a>
-                    <a class="btn btn-sm btn-outline-light" href="#">Sign up</a>
-                </div>
-            </div>
-        </div>
-    </header>
+    <?php require('templates/navigation.php'); ?>
 
     <div class="container">
-        <div class="nav-scroller py-1 mb-2">
-            <nav class="nav d-flex justify-content-around">
-                <a class="p-2 text-muted" href="allBooks.html">Books</a>
-                <a class="p-2 text-muted" href="">Movies</a>
-            </nav>
-        </div>
+        <?php require('templates/navscroller.php'); ?>
 
         <div class="row mb-2">
             <div class="col">
@@ -87,9 +53,6 @@
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/script.js"></script>
+    <?php require('templates/footer.php'); ?>
 </body>
 </html>
---
+++ templates/navscroller.php
@@ -0,0 +1,6 @@
+<div class="nav-scroller py-1 mb-2">
+    <nav class="nav d-flex justify-content-around">
+        <a class="p-2 text-muted" href="allBooks.php">Books</a>
+        <a class="p-2 text-muted" href="">Movies</a>
+    </nav>
+</div>
